82682c73812703675223b82d5e66d268c114ecd9
= <alec.hoyland@gmail.com>
2018-11-02 14:31:42 -0400

    Use calcium mechanisms

--- bursting-single-compartment-neurons/liu/make_neuron_liu.m
+++ bursting-single-compartment-neurons/liu/make_neuron_liu.m
@@ -10,13 +10,8 @@ r = 0.0707;
 vol = (4/3)*pi*(r^3);
 A = 4*pi*r*r;
 
-f = 14.96; % uM/nA
-tau_Ca = 200;
-F = 96485; % Faraday constant in SI units
-phi = (2*f*F*vol)/tau_Ca;
-
 x = xolotl;
-x.add('AB','compartment','V',-65,'Ca',0.02,'Cm',10,'A',A,'vol',vol,'phi',phi,'Ca_out',3000,'Ca_in',0.05,'tau_Ca',tau_Ca);
+x.add('compartment', 'AB','V',-65,'Ca',0.02,'Cm',10,'A',A,'vol',vol,'Ca_out',3000);
 x.AB.add('liu-approx/NaV','gbar',1000,'E',30);
 x.AB.add('liu-approx/CaT','gbar',25,'E',30);
 x.AB.add('liu-approx/CaS','gbar',60,'E',30);
@@ -25,6 +20,7 @@ x.AB.add('liu-approx/KCa','gbar',50,'E',-80);
 x.AB.add('liu-approx/Kd','gbar',1000,'E',-80);
 x.AB.add('liu-approx/HCurrent','gbar',.1,'E',-20);
 x.AB.add('Leak','gbar',.1,'E',-50);
+x.AB.add('CalciumMech1');
 
 x.t_end = 20e3;
 x.dt = .1;
