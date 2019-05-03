bceab0896e7cf4ee342653bab7e58435f198a387
gitlab@srinivas.gs <code@srinivas.gs>
2019-05-03 16:51:32 -0400

    ready to deploy

--- bursting-two-comp-model/fit_PD.m
+++ bursting-two-comp-model/fit_PD.m
@@ -1,4 +1,7 @@
 
+
+function fit_PD(MaxTime,n_epochs)
+
 load('../exact-fit/isolated_PD')
 V0 = V;
 T = 1e3;
@@ -39,5 +42,54 @@ p.options.UseParallel = true;
 
 p.sim_func = @metricsCost;
 
-p.seed = rand(length(p.seed),1).*(p.ub - p.lb) + p.lb;
 
+
+% run forever, churning out neurons
+N = 1e4;
+all_params = NaN(N,length(p.ub));
+all_cost = NaN(N,1);
+
+file_name = 'PD_models.mat';
+
+if exist(file_name)
+	load(file_name)
+	start_idx = find(isnan(all_cost),1,'first');
+else
+	start_idx = 1;
+end
+
+p.options.MaxTime = MaxTime;
+p.options.Display = 'iter';
+
+
+for i = start_idx:N
+	disp(['Starting with random seed #' strlib.oval(i)])
+	try
+		p.seed = rand(length(p.seed),1).*(p.ub - p.lb) + p.lb;
+		for j = 1:n_epochs
+			p.fit;
+		end
+
+		% save
+		x.set(p.parameter_names,p.seed);
+		this_cost = p.sim_func(x, p.data);
+
+		if this_cost < .5
+
+
+			disp(['Final Cost for this seed is ' strlib.oval(all_cost(i))])
+
+			all_params(i,:) = p.seed;
+			all_cost(i) = this_cost;
+
+			save(file_name,'all_params','all_cost')
+		else
+			disp('Cost too high, skipping...')
+		end
+
+	catch
+		disp('Something went wrong. Ouch. ')
+	end
+
+
+end
